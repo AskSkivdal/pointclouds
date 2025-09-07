4fb42bb3188f1d4d78824117e2d97c40eea3db43
AskSkivdal <mail@askskivdal.com>
2025-09-07 23:28:10 +0200

    Add a lot of tests that will always fail.

---
+++ pointcloud/tests/test_pointcloud.rs
@@ -0,0 +1,89 @@
+#[cfg(test)]
+mod stats {
+    #[test]
+    fn is_indexed() {
+        todo!()
+    }
+
+    #[test]
+    fn point_count() {
+        todo!()
+    }
+
+    #[test]
+    fn get_bounds() {
+        todo!()
+    }
+
+    #[test]
+    fn get_bounds_miss() {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod modify {
+
+    #[test]
+    fn add_point() {
+        todo!()
+    }
+
+    #[test]
+    fn remove_point() {
+        todo!()
+    }
+
+    #[test]
+    fn add_points() {
+        todo!()
+    }
+
+    #[test]
+    fn remove_points() {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod query {
+    #[test]
+    fn get_by_distance_miss() {
+        todo!()
+    }
+
+    #[test]
+    fn get_by_distance_hit() {
+        todo!()
+    }
+
+    #[test]
+    fn get_in_bbox_miss() {
+        todo!()
+    }
+
+    #[test]
+    fn get_in_bbox_hit() {
+        todo!()
+    }
+
+    #[test]
+    fn get_not_in_bbox_miss() {
+        todo!()
+    }
+
+    #[test]
+    fn get_not_in_bbox_hit() {
+        todo!()
+    }
+
+    #[test]
+    fn get_closest_miss() {
+        todo!()
+    }
+
+    #[test]
+    fn get_closest_hit() {
+        todo!()
+    }
+}
